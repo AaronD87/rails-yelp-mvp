f647c4f68e9bd633559e5ec90510d879f5d5a7be
Aaron Dennis <AMDennis1987@gmail.com>
2017-11-29 14:20:38 +0100

    satisfy rake

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Started seeds"
+puts "Deleting database"
+
+Restaurant.destroy_all
+
+category = %w("chinese", "italian", "japanese", "french", "belgian")
+
+20.times do
+  restaurants = Restaurant.new(title: Faker::Overwatch.hero, content: Faker::Overwatch.quote)
+
+  puts "#{articles.title} saved!" if articles.save
+end
+
+puts "Seeding finished!"
